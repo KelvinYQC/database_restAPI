779bf3a84a404807bef63ddc29033849677d5034
Kelvin <yiqing.cheng@emory.edu>
2021-12-03 22:56:33 -0500

    temp commit for having working session

---
+++ db.js
@@ -0,0 +1,10 @@
+// const dotenv = require("dotenv")
+// dotenv.config()
+// const mongodb = require("mongodb")
+// const mongoose = require("mongoose");
+
+// mongoose.connect(
+//   process.env.DB_CONNECTION, 
+//  {useNewUrlParser:true},
+//   () =>console.log("database is conncted")    
+// );
